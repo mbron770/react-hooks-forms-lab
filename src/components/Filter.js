a3f0804feec7f7a6210ffe4f9b2e050dce772308
mbron770 <mbronfin.tsd@gmail.com>
2023-06-18 03:20:44 -0400

    complete deliverable

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, search, setSearch}) {
+  
+
+  function handleSearch(e){
+    setSearch(e.target.value)
+  }
+  
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" 
+      name="search" 
+      placeholder="Search..."
+      value = {search} 
+      onChange = {handleSearch}
+      
+      
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,22 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, addItemForm }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState('')
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items.filter((item) => selectedCategory === 
+  "All" || item.category === selectedCategory).filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm addItemForm = {addItemForm}/>
+      <Filter onCategoryChange={handleCategoryChange} search = {search} setSearch = {setSearch} />
+       
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
